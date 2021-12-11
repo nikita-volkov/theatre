a8b616df1f75ffb5903dd09cd0bf117002918a0d
Nikita Volkov <nikita.y.volkov@mail.ru>
2021-12-11 14:06:23 +0300

    Modernise

--- library/Theatre.hs
+++ library/Theatre.hs
@@ -1,36 +1,33 @@
-{-|
-Minimalistic actor library.
--}
+-- |
+-- Minimalistic actor library.
 module Theatre
-(
-  Actor,
-  -- * Construction
-  graceful,
-  disgraceful,
-  suicidal,
-  steppingEffectfully,
-  steppingPurely,
-  -- * Usage
-  tell,
-  kill,
-)
+  ( Actor,
+
+    -- * Construction
+    graceful,
+    disgraceful,
+    suicidal,
+    steppingEffectfully,
+    steppingPurely,
+
+    -- * Usage
+    tell,
+    kill,
+  )
 where
 
-import Theatre.Prelude
 import qualified Control.Concurrent.Chan.Unagi as E
 import qualified SlaveThread as F
+import Theatre.Prelude
 
-
-{-|
-Actor, which processes the messages of type @message@.
-
-An abstraction over the message channel, thread-forking and killing.
--}
-data Actor message =
-  Actor {
-    {-| Send a message to the actor -}
+-- |
+-- Actor, which processes the messages of type @message@.
+--
+-- An abstraction over the message channel, thread-forking and killing.
+data Actor message = Actor
+  { -- | Send a message to the actor
     tell :: message -> IO (),
-    {-| Kill the actor -}
+    -- | Kill the actor
     kill :: IO ()
   }
 
@@ -76,91 +73,97 @@ instance Decidable Actor where
       kill =
         leftKill >> rightKill
 
-{-|
-An actor which cannot die by itself unless explicitly killed.
-
-Given an interpreter of messages,
-forks a thread to run the computation on and 
-produces a handle to address that actor.
-
-Killing that actor will make it process all the messages in the queue first.
-All the messages sent to it after killing won't be processed.
--}
-graceful :: (message -> IO ()) {-^ Interpreter of a message -} -> IO (Actor message)
+-- |
+-- An actor which cannot die by itself unless explicitly killed.
+--
+-- Given an interpreter of messages,
+-- forks a thread to run the computation on and
+-- produces a handle to address that actor.
+--
+-- Killing that actor will make it process all the messages in the queue first.
+-- All the messages sent to it after killing won't be processed.
+graceful ::
+  -- | Interpreter of a message
+  (message -> IO ()) ->
+  IO (Actor message)
 graceful interpretMessage =
   do
     (inChan, outChan) <- E.newChan
-    F.fork $ fix $ \loop -> {-# SCC "graceful/loop" #-} do
-      message <- E.readChan outChan
-      case message of
-        Just payload ->
-          do
-            interpretMessage payload
-            loop
-        Nothing ->
-          return ()
+    F.fork $
+      fix $ \loop ->
+        {-# SCC "graceful/loop" #-}
+        do
+          message <- E.readChan outChan
+          case message of
+            Just payload ->
+              do
+                interpretMessage payload
+                loop
+            Nothing ->
+              return ()
     return (Actor (E.writeChan inChan . Just) (E.writeChan inChan Nothing))
 
-{-|
-An actor which cannot die by itself unless explicitly killed.
-
-Given an interpreter of messages,
-forks a thread to run the computation on and 
-produces a handle to address that actor.
--}
-disgraceful :: (message -> IO ()) {-^ Interpreter of a message -} -> IO (Actor message)
+-- |
+-- An actor which cannot die by itself unless explicitly killed.
+--
+-- Given an interpreter of messages,
+-- forks a thread to run the computation on and
+-- produces a handle to address that actor.
+disgraceful ::
+  -- | Interpreter of a message
+  (message -> IO ()) ->
+  IO (Actor message)
 disgraceful receiver =
   suicidal (\producer -> forever (producer >>= receiver))
 
-{-|
-An actor, whose interpreter can decide that the actor should die.
-
-Given an implementation of a receiver loop of messages,
-forks a thread to run that receiver on and
-produces a handle to address that actor.
--}
-suicidal :: (IO message -> IO ()) {-^ A message receiver loop. When the loop exits, the actor dies -} -> IO (Actor message)
+-- |
+-- An actor, whose interpreter can decide that the actor should die.
+--
+-- Given an implementation of a receiver loop of messages,
+-- forks a thread to run that receiver on and
+-- produces a handle to address that actor.
+suicidal ::
+  -- | A message receiver loop. When the loop exits, the actor dies
+  (IO message -> IO ()) ->
+  IO (Actor message)
 suicidal receiver =
   do
     (inChan, outChan) <- E.newChan
     threadId <- F.fork (receiver (E.readChan outChan))
     return (Actor (E.writeChan inChan) (killThread threadId))
 
-{-|
-An actor which threads a persistent state thru its iterations.
-It cannot die by itself unless explicitly killed.
-
-Given an interpreter of messages and initial state generator,
-forks a thread to run the computation on and 
-produces a handle to address that actor.
-
-Killing that actor will make it process all the messages in the queue first.
-All the messages sent to it after killing won't be processed.
--}
+-- |
+-- An actor which threads a persistent state thru its iterations.
+-- It cannot die by itself unless explicitly killed.
+--
+-- Given an interpreter of messages and initial state generator,
+-- forks a thread to run the computation on and
+-- produces a handle to address that actor.
+--
+-- Killing that actor will make it process all the messages in the queue first.
+-- All the messages sent to it after killing won't be processed.
 steppingEffectfully :: (message -> state -> IO state) -> IO state -> IO (Actor message)
 steppingEffectfully transition initalize =
   do
     (inChan, outChan) <- E.newChan
-    let
-      loop !state =
-        {-# SCC "steppingEffectfully/loop" #-}
-        do
-          message <- E.readChan outChan
-          case message of
-            Just payload ->
-              do
-                newState <- transition payload state
-                loop newState
-            Nothing ->
-              return ()
+    let loop !state =
+          {-# SCC "steppingEffectfully/loop" #-}
+          do
+            message <- E.readChan outChan
+            case message of
+              Just payload ->
+                do
+                  newState <- transition payload state
+                  loop newState
+              Nothing ->
+                return ()
     F.fork (initalize >>= loop)
     return (Actor (E.writeChan inChan . Just) (E.writeChan inChan Nothing))
 
-{-|
-Same as 'steppingEffectfully',
-but isolates pure state management from its effectful interpretation,
-letting you define the logic in isolation.
--}
+-- |
+-- Same as 'steppingEffectfully',
+-- but isolates pure state management from its effectful interpretation,
+-- letting you define the logic in isolation.
 steppingPurely :: (message -> state -> state) -> state -> (state -> IO ()) -> IO (Actor message)
 steppingPurely transition initialState act =
   steppingEffectfully (\m s -> actReturning (transition m s)) (actReturning initialState)
